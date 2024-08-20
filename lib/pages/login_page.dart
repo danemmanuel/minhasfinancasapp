778d25471a8ec1d0247843849151b4a8408e824a
danemmanuel <danemmanuelturibio@gmail.com>
2024-08-20 13:48:31 -0300

    login google

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'contas_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'contas_page.dart';
 
 class LoginApp extends StatelessWidget {
   @override
@@ -28,6 +28,7 @@ Future<void> saveAuthToken(String token) async {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<void> _authenticateUser(String email, String password) async {
     final url =
@@ -55,6 +56,57 @@ class _LoginPageState extends State<LoginPage> {
     } else {
       // Autenticação falhou
       print('Falha na autenticação. Código de status: ${response.statusCode}');
+      // Exibir mensagem de erro ao usuário
+    }
+  }
+
+  Future<void> _googleSignInMethod() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        // O login foi cancelado pelo usuário
+        return;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final String? idToken = googleAuth.idToken;
+      print('ID Token: $idToken');
+      if (idToken != null) {
+        // Enviar o token para o backend para autenticação
+        final url =
+            Uri.parse('https://financess-back.herokuapp.com/auth/google/auth');
+        final response = await http.post(
+          url,
+          body: json.encode({'idToken': idToken}),
+          headers: {'Content-Type': 'application/json'},
+        );
+
+        if (response.statusCode == 201) {
+          // Autenticação bem-sucedida
+          Map<String, dynamic> responseBody = json.decode(response.body);
+          String? accessToken = responseBody['accessToken'];
+          print('Resposta do backend: ${response.body}');
+          if (accessToken != null) {
+            print('Autenticação bem-sucedida. Token: $accessToken');
+            await saveAuthToken(accessToken);
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          } else {
+            print('Erro: accessToken é nulo');
+          }
+        } else {
+          print(
+              'Erro na resposta do backend: ${response.statusCode} ${response.body}');
+        }
+      } else {
+        print('Erro: idToken é nulo');
+      }
+    } catch (error) {
+      print('Erro no login com Google: $error');
       // Exibir mensagem de erro ao usuário
     }
   }
@@ -91,6 +143,11 @@ class _LoginPageState extends State<LoginPage> {
                     _emailController.text, _passwordController.text);
               },
               child: Text('Login'),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _googleSignInMethod,
+              child: Text('Login with Google'),
             ),
           ],
         ),
